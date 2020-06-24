8d9c8674cad0ee315347ff6f0dbbce80860cb8fd
Tej <tejpartapsingh91@gmail.com>
2020-06-24 17:48:16 -0400

    🐛 FIX:revise table border, add error table, fix star emoji spacing

--- utils/footer.js
+++ utils/footer.js
@@ -2,6 +2,12 @@ const { cyan, dim } = require('chalk');
 const io = require('console-read-write');
 
 module.exports = () => {
-    io.write(dim(cyan('\n Star ⭐️ the repo: https://github.com/msaaddev/geo-location-cli/')));
-    io.write(dim(cyan(' Connect with me: https://twitter.com/MSaaddev/\n')));
+  io.write(
+    dim(
+      cyan(
+        '\nStar ⭐️  the repo: https://github.com/msaaddev/geo-location-cli/'
+      )
+    )
+  );
+  io.write(dim(cyan('Connect with me: https://twitter.com/MSaaddev/\n')));
 };
--- utils/table.js
+++ utils/table.js
@@ -2,21 +2,23 @@ const cliTable = require('cli-table');
 const colors = require('colors');
 
 module.exports = (data) => {
-    const table = new cliTable({ head: ['Location Information'.yellow] });
+  const table = new cliTable({
+    head: ['Location Information'.yellow, 'Result'.yellow],
+  });
 
-    table.push(
-        { Continent: data.continent },
-        { 'Country Name': data.name },
-        { 'Country Code': data.country_code },
-        { Nationality: data.nationality },
-        { 'International Olympic Committee': data.ioc },
-        { 'Currency Code': data.currency_code },
-        { 'World Region': data.world_region },
-        { Subregion: data.subregion },
-        { 'European Union Member': data.eu_member },
-        { 'Language Spoken': data.languages_spoken },
-        { 'Start Of Week': data.start_of_week }
-    );
+  table.push(
+    { Continent: data.continent },
+    { 'Country Name': data.name },
+    { 'Country Code': data.country_code },
+    { Nationality: data.nationality },
+    { 'International Olympic Committee': data.ioc },
+    { 'Currency Code': data.currency_code },
+    { 'World Region': data.world_region },
+    { Subregion: data.subregion },
+    { 'European Union Member': data.eu_member },
+    { 'Language Spoken': data.languages_spoken },
+    { 'Start Of Week': data.start_of_week }
+  );
 
-    console.log(table.toString());
+  console.log(table.toString());
 };
