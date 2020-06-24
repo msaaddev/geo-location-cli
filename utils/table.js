ddb59c782d555ff22136d5c3dace992ab3b605c7
msaaddev <mrsaadirfan@gmail.com>
2020-06-24 12:11:53 +0500

    📦 NEW: table

---
+++ utils/table.js
@@ -0,0 +1,22 @@
+const cliTable = require('cli-table');
+const colors = require('colors');
+
+module.exports = (data) => {
+    const table = new cliTable({ head: ['Location Information'.yellow] });
+
+    table.push(
+        { Continent: data.continent },
+        { 'Country Name': data.name },
+        { 'Country Code': data.country_code },
+        { Nationality: data.nationality },
+        { 'International Olympic Committee': data.ioc },
+        { 'Currency Code': data.currency_code },
+        { 'World Region': data.world_region },
+        { Subregion: data.subregion },
+        { 'European Union Member': data.eu_member },
+        { 'Language Spoken': data.languages_spoken },
+        { 'Start Of Week': data.start_of_week }
+    );
+
+    console.log(table.toString());
+};
