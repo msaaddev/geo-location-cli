8ebc4a92a8d896a90657906d4ca72bcfd459ab12
msaaddev <mrsaadirfan@gmail.com>
2020-06-24 12:45:31 +0500

    🐛 FIX: url

--- utils/footer.js
+++ utils/footer.js
@@ -2,6 +2,6 @@ const { cyan, dim } = require('chalk');
 const io = require('console-read-write');
 
 module.exports = () => {
-    io.write(dim(cyan('\n Star ⭐️ the repo: https://github.com/msaaddev/location-cli/')));
+    io.write(dim(cyan('\n Star ⭐️ the repo: https://github.com/msaaddev/geo-location-cli/')));
     io.write(dim(cyan(' Connect with me: https://twitter.com/MSaaddev/\n')));
 };
