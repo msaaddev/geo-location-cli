5f1af1b31701f7706c929b61755d0bf1b8a21abb
msaaddev <mrsaadirfan@gmail.com>
2020-06-27 00:14:16 +0500

    🐛 FIX: languages spoken

--- utils/table.js
+++ utils/table.js
@@ -1,25 +1,36 @@
-const cliTable = require("cli-table");
-const colors = require("colors");
+const cliTable = require('cli-table');
+const colors = require('colors');
 
 module.exports = (data) => {
-  const table = new cliTable({
-    head: ["Location Information".yellow, "Result".yellow],
-  });
+    const table = new cliTable({
+        head: ['Location Information'.yellow, 'Result'.yellow],
+    });
 
-  table.push(
-    { Continent: data.continent },
-    { "Country Name": data.name },
-    { "Country Code": data.country_code },
-    { Nationality: data.nationality },
-    { "International Olympic Committee": data.ioc },
-    { "Currency Code": data.currency_code },
-    { "World Region": data.world_region },
-    { Subregion: data.subregion },
-    { "Language Spoken": data.languages_spoken },
-    { "Start Of Week": data.start_of_week },
-    { "Latitude": data.geo.latitude },
-    { "Longitude": data.geo.longitude },
-  );
+    let languages = '';
 
-  console.log(table.toString());
+    for (let i = 0, j = 0; i < data.languages_spoken.length; i++) {
+        languages += data.languages_spoken[i];
+        j = i;
+        if (j++ === data.languages_spoken.length - 1) break;
+        else languages += ', ';
+    }
+
+    console.log(languages);
+
+    table.push(
+        { Continent: data.continent },
+        { 'Country Name': data.name },
+        { 'Country Code': data.country_code },
+        { Nationality: data.nationality },
+        { 'International Olympic Committee': data.ioc },
+        { 'Currency Code': data.currency_code },
+        { 'World Region': data.world_region },
+        { Subregion: data.subregion },
+        { 'Language Spoken': languages },
+        { 'Start Of Week': data.start_of_week },
+        { Latitude: data.geo.latitude },
+        { Longitude: data.geo.longitude }
+    );
+
+    console.log(table.toString());
 };
