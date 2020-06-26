02c0b37073f12a5da3da7d50b02f51d32bcc73c7
msaaddev <mrsaadirfan@gmail.com>
2020-06-26 23:55:36 +0500

    📦 NEW: latitudes & longitudes

--- utils/table.js
+++ utils/table.js
@@ -1,23 +1,24 @@
-const cliTable = require('cli-table');
-const colors = require('colors');
+const cliTable = require("cli-table");
+const colors = require("colors");
 
 module.exports = (data) => {
   const table = new cliTable({
-    head: ['Location Information'.yellow, 'Result'.yellow],
+    head: ["Location Information".yellow, "Result".yellow],
   });
 
   table.push(
     { Continent: data.continent },
-    { 'Country Name': data.name },
-    { 'Country Code': data.country_code },
+    { "Country Name": data.name },
+    { "Country Code": data.country_code },
     { Nationality: data.nationality },
-    { 'International Olympic Committee': data.ioc },
-    { 'Currency Code': data.currency_code },
-    { 'World Region': data.world_region },
+    { "International Olympic Committee": data.ioc },
+    { "Currency Code": data.currency_code },
+    { "World Region": data.world_region },
     { Subregion: data.subregion },
-    { 'European Union Member': data.eu_member },
-    { 'Language Spoken': data.languages_spoken },
-    { 'Start Of Week': data.start_of_week }
+    { "Language Spoken": data.languages_spoken },
+    { "Start Of Week": data.start_of_week },
+    { "Latitude": data.geo.latitude },
+    { "Longitude": data.geo.longitude },
   );
 
   console.log(table.toString());
