f2e8e624693afbabf3b039b56f11a1b500551c7f
msaaddev <mrsaadirfan@gmail.com>
2020-06-26 23:55:23 +0500

    👌 IMPROVE: spacing

--- utils/cli.js
+++ utils/cli.js
@@ -1,26 +1,27 @@
-const axios = require('axios');
-const io = require('console-read-write');
-const chalk = require('chalk');
-const ora = require('ora');
-const table = require('./table');
-const cliTable = require('cli-table');
+const axios = require("axios");
+const io = require("console-read-write");
+const chalk = require("chalk");
+const ora = require("ora");
+const table = require("./table");
+const cliTable = require("cli-table");
 
 module.exports = async () => {
-  io.write(chalk.green('\nEnter any IP Address: '));
+  io.write(chalk.green("\nEnter any IP Address: "));
   const ip = await io.read();
-  const spinner = ora(' Fetching Location Information');
+  const spinner = ora(" Fetching Location Information");
   try {
-    io.write('');
+    io.write("");
     spinner.start();
     const { data } = await axios.get(
-      `https://api.ipgeolocationapi.com/geolocate/${ip}`
+      `https://api.ipgeolocationapi.com/geolocate/${ip}`,
     );
+
     spinner.stop();
     table(data);
   } catch (error) {
     spinner.stop();
     const errorTable = new cliTable();
-    errorTable.push({ Error: 'Invalid IP Address' });
+    errorTable.push({ Error: "Invalid IP Address" });
     console.log(errorTable.toString());
   }
 };
